6ac3d82f818ab3b24cf3e191d4494ee5dbdf76c1
dark1922 <jvdematos004@gmail.com>
2021-11-08 20:47:03 -0300

    adicionando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Comp1Component } from './components/comp1/comp1.component';
-import { Comp2Component } from './components/comp2/comp2.component';
-import { CompInteracaoComponent } from './components/comp1/comp-interacao/comp-interacao.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { BootstrapModule } from './bootstrap/bootstrap.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Comp1Component,
-    Comp2Component,
-    CompInteracaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BootstrapModule //ultiza os modulos do bootstrap que estiverem aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
